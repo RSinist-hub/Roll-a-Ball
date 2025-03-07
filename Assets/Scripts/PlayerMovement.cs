3e3599530b27e18377626e8a53098947e287906a
RSinist-hub <sindledecker@gmail.com>
2025-03-06 22:25:18 -0800

    Added jump and follow camera
    Lesson learned: Cinemachine camera was "jerky" so I had to change the Main Camera's Cinemachine Brain component's Update Method to "Fixed Update"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpForce = 5f;
     private Rigidbody rb;
+    private bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,12 +14,32 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            //Debug.Log("Jump!");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
     void FixedUpdate()
     {
+        // Get input
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+       
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
+        // rb.AddForce(movement * speed, ForceMode.VelocityChange);
+    }
+
+    // Check if player is touching the ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
